484c1f73bfd6a3bcff32ebf702b1e614f06a81b8
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-07-03 12:52:18 -0500

    update pitch

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
-pub mod accidental;
-pub mod note;
 pub mod pitch;
 pub mod tempo;
 pub mod time_signature;
 pub mod dynamic;
 
-pub use accidental::Accidental;
-pub use note::Note;
 pub use pitch::Pitch;
 pub use tempo::Tempo;
 pub use time_signature::TimeSignature;
--- src/pitch.rs
+++ src/pitch.rs
@@ -1,10 +1,30 @@
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Display, Formatter, Write};
 
 use thiserror::Error;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pitch {
+    pitch: Name,
+    accidental: Accidental,
+    octave: u8,
+}
+
+// Todo: Setters maybe
+impl Pitch {
+    pub fn name(&self) -> Name {
+        self.pitch
+    }
+    pub fn accidental(&self) -> Accidental {
+        self.accidental
+    }
+    pub fn octave(&self) -> u8 {
+        self.octave
+    }
+}
+
 #[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd)]
-pub enum Pitch {
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, strum::Display)]
+pub enum Name {
     A,
     B,
     C,
@@ -14,39 +34,71 @@ pub enum Pitch {
     G,
 }
 
-impl TryFrom<char> for Pitch {
-    type Error = Error;
+impl TryFrom<char> for Name {
+    type Error = NameError;
     fn try_from(value: char) -> Result<Self, Self::Error> {
         match value.to_ascii_lowercase() {
-            'a' => Ok(Pitch::A),
-            'b' => Ok(Pitch::B),
-            'c' => Ok(Pitch::C),
-            'd' => Ok(Pitch::D),
-            'e' => Ok(Pitch::E),
-            'f' => Ok(Pitch::F),
-            'g' => Ok(Pitch::G),
-            _ => Err(Error(value)),
+            'a' => Ok(Name::A),
+            'b' => Ok(Name::B),
+            'c' => Ok(Name::C),
+            'd' => Ok(Name::D),
+            'e' => Ok(Name::E),
+            'f' => Ok(Name::F),
+            'g' => Ok(Name::G),
+            _ => Err(NameError(value)),
         }
+    }
+}
+
+#[repr(u8)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Accidental {
+    #[default]
+    Natural,
+
+    Sharp,
+    Flat,
+}
+
+impl Display for Accidental {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Accidental::Natural => '♮',
+            Accidental::Sharp => '♯',
+            Accidental::Flat => '♭',
+        };
+        f.write_char(c)
     }
 }
 
 impl Display for Pitch {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        // debug is same as display
-        Debug::fmt(self, f)
+        Display::fmt(&self.pitch, f)?;
+        Display::fmt(&self.accidental, f)?;
+        Display::fmt(&self.octave, f)
     }
 }
 
 #[derive(Error, Debug)]
-#[error("invalid pitch \"{0}\"")]
-pub struct Error(char);
+#[error("invalid pitch name \"{0}\"")]
+pub struct NameError(char);
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_from_char() {
-        assert!(matches!(Pitch::try_from('h'), Err(Error('h'))));
-        assert!(matches!(Pitch::try_from('a'), Ok(Pitch::A)));
+    fn test_name_from_char() {
+        assert!(matches!(Name::try_from('h'), Err(NameError('h'))));
+        assert!(matches!(Name::try_from('a'), Ok(Name::A)));
+    }
+
+    #[test]
+    fn test_pitch() {
+        let note = Pitch {
+            pitch: Name::A,
+            accidental: Accidental::Flat,
+            octave: 4,
+        };
+        assert_eq!(format!("{note}"), "A♭4");
     }
 }
